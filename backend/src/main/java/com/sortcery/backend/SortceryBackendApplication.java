d35c74216a3f3ae66a02a3c5879aac7e3fc9d215
PeppermintSnow <168705921+PeppermintSnow@users.noreply.github.com>
2026-05-15 18:30:05 +0800

    init: backend and frontend template initialization

---
+++ backend/src/main/java/com/sortcery/backend/SortceryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sortcery.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortceryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortceryBackendApplication.class, args);
+	}
+
+}
---
+++ frontend/android/app/src/main/kotlin/com/example/sortcery/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sortcery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
