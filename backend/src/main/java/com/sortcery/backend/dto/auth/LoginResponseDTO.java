7371be14a5e0152afea33dfe7a87ce792af0cbe9
PeppermintSnow <168705921+PeppermintSnow@users.noreply.github.com>
2026-05-21 01:57:24 +0800

    feat(backend): add name fields on login response

--- backend/src/main/java/com/sortcery/backend/dto/auth/LoginResponseDTO.java
+++ backend/src/main/java/com/sortcery/backend/dto/auth/LoginResponseDTO.java
@@ -5,18 +5,24 @@ import com.sortcery.backend.model.User;
 public class LoginResponseDTO {
     private Long id;
     private String email;
+    private String firstName;
+    private String lastName;
     private User.Role role;
     private String plainToken;
 
     public LoginResponseDTO(User user, String plainToken) {
         this.id = user.getId();
         this.email = user.getEmail();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
         this.role = user.getRole();
         this.plainToken = plainToken;
     }
 
     public Long getId() { return id; }
     public String getEmail() { return email; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
     public User.Role getRole() { return role; }
     public String getPlainToken() { return plainToken; }
 }
