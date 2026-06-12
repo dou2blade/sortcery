51ea1f94de6cf72276f786b7183154ba820d97aa
PeppermintSnow <168705921+PeppermintSnow@users.noreply.github.com>
2026-06-12 10:09:41 +0800

    feat(backend): add product name to variant dto

--- backend/src/main/java/com/sortcery/backend/dto/productvariant/ProductVariantResponseDTO.java
+++ backend/src/main/java/com/sortcery/backend/dto/productvariant/ProductVariantResponseDTO.java
@@ -7,6 +7,7 @@ public class ProductVariantResponseDTO {
     private final Long id;
     private final Long productId;
     private final String name;
+    private final String productName;
     private final String imageUrl;
     private final LocalDateTime createdAt;
     private final LocalDateTime updatedAt;
@@ -15,6 +16,7 @@ public class ProductVariantResponseDTO {
         this.id = productVariant.getId();
         this.productId = productVariant.getProduct().getId();
         this.name = productVariant.getName();
+        this.productName = productVariant.getProduct().getName();
         this.imageUrl = productVariant.getImageUrl();
         this.createdAt = productVariant.getCreatedAt();
         this.updatedAt = productVariant.getUpdatedAt();
@@ -23,6 +25,7 @@ public class ProductVariantResponseDTO {
     public Long getId() { return id; }
     public Long getProductId() { return productId; }
     public String getName() { return name; }
+    public String getProductName() { return productName; }
     public String getImageUrl() { return imageUrl; }
     public LocalDateTime getCreatedAt() { return createdAt; }
     public LocalDateTime getUpdatedAt() { return updatedAt; }
