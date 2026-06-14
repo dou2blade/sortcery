f88110dcd5f28b0a057e6fbfa70b229c4e056d7b
PeppermintSnow <168705921+PeppermintSnow@users.noreply.github.com>
2026-06-15 03:10:40 +0800

    feat(frontend): store/branch list

--- backend/src/main/java/com/sortcery/backend/controller/PublicBranchController.java
+++ backend/src/main/java/com/sortcery/backend/controller/PublicBranchController.java
@@ -1,8 +1,12 @@
 package com.sortcery.backend.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +28,50 @@ public class PublicBranchController {
         this.branchService = branchService;
     }
 
+    @GetMapping
+    public ResponseEntity<ApiResponse> findAll(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "15") int size,
+        @RequestParam(required = false) String search,
+        @RequestParam(required = false) Long store,
+        @RequestParam(required = false) Double latitude,
+        @RequestParam(required = false) Double longitude
+    ) {
+        Sort sort = Sort.by("createdAt").descending();
+        
+        List<Map<String, Object>> branches = branchService.findPage(page, size, search, store, sort)
+            .stream()
+            .map(branch -> {
+                Map<String, Object> data = new HashMap<>();
+
+                data.put("id", branch.getId());
+                data.put("name", branch.getName());
+                data.put("storeId", branch.getStoreId());
+                data.put("storeName", branch.getStoreName());
+                data.put("address", branch.getAddress());
+
+                if (latitude != null && longitude != null) {
+                    data.put(
+                        "distance",
+                        Haversine.distance(
+                            latitude,
+                            longitude,
+                            branch.getLatitude(),
+                            branch.getLongitude()
+                        )
+                    );
+                }
+
+                return data;
+            })
+            .toList();
+
+        
+
+        return ResponseEntity.ok(ApiResponse.of(branches));
+    }
+
+
     @GetMapping(path="/{id}")
     public ResponseEntity<ApiResponse> findById(
         @PathVariable Long id,
@@ -39,6 +87,8 @@ public class PublicBranchController {
         data.put("storeId", branch.getStoreId());
         data.put("storeName", branch.getStoreName());
         data.put("address", branch.getAddress());
+        data.put("latitude", branch.getLatitude());
+        data.put("longitude", branch.getLongitude());
 
         if (latitude != null && longitude != null) {
             data.put(
